cd28dd741bdc45598c51f2307ac4bade4c53ccbc
Sassan Bahramirad <Sassan Bahramirad>
2016-12-16 12:16:28 -0800

    testing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var port  	 = process.env.PORT || 8080; 				// set the port
 //var database = require('./server/config/config'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+//var methodOverride = require('method-override');
 
 
 
@@ -23,7 +23,7 @@ app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+//app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 
 // setup config first before anything by requiring it
